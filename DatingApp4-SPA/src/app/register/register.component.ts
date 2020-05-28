749df46a0918e6e1046240a9cfad0d68f73c04c1
vijay babar <vijbab.kalp@gmail.com>
2020-05-29 00:16:33 +0530

    end of section 6 Adding 3rd party components to our app

--- DatingApp4-SPA/src/app/register/register.component.ts
+++ DatingApp4-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,20 +12,19 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() { }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('Canceled');
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('Registration successfull');
+      this.alertify.success('Registration successfull');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 }
